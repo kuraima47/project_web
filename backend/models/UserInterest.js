448c33fa8b458e934346bc30c8619302c43b154f
emilien <gallonemilien@gmail.com>
2025-01-18 23:47:57 +0000

    fix small bug

--- backend/models/UserInterest.js
+++ backend/models/UserInterest.js
@@ -1,8 +1,8 @@
 // models/userInterest.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const User = require('./user');
-const Interest = require('./interest');
+const User = require('./User');
+const Interest = require('./Interest');
 
 const UserInterest = sequelize.define('UserInterest', {
     userId: {
